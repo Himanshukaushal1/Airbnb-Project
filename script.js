c1ae7474d0c66cd02b32d696ad51ff2476c9515b
Himanshukaushal <himanshukaushal779@gmail.com>
2024-09-08 17:55:46 +0530

    Complete footer section

---
+++ script.js
@@ -0,0 +1,4 @@
+// window.addEventListener('scroll',()=>{
+//     document.querySelector('.mode').classList.add('delete-mode');
+//     document.querySelector('.mid').classList.add('delete-mid');
+// });
