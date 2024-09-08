0e7f8247cbc9f12be67ab3c9b82bffb33ef1e3e9
Himanshukaushal <himanshukaushal779@gmail.com>
2024-09-08 20:15:42 +0530

    Add sroll effect

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-// window.addEventListener('scroll',()=>{
-//     document.querySelector('.mode').classList.add('delete-mode');
-//     document.querySelector('.mid').classList.add('delete-mid');
-// });
+ window.addEventListener('scroll',()=>{
+    if(window.scrollY >50){
+        document.querySelector('.mode').classList.add('delete-mode');
+        document.querySelector('.mid').classList.add('delete-mid');
+        document.querySelector('.new-nav-container').classList.remove('hide-new-nav');
+    }
+    else{
+        document.querySelector('.mode').classList.remove('delete-mode');
+        document.querySelector('.mid').classList.remove('delete-mid');
+        document.querySelector('.new-nav-container').classList.add('hide-new-nav');
+    }
+ });
